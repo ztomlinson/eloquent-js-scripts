787681e3349d45ccbf7f292a3f518a17bdc62fe0
Zachary Tomlinson <Zachary Tomlinson>
2017-05-23 12:38:10 -0400

    reduce function practice

---
+++ ch5-reduce.js
@@ -0,0 +1,10 @@
+function reduce(array, combine, start) {
+  var current = start;
+  for (var i = 0; i < array.length; i++)
+    current = combine(current, array[i]);
+  return current;
+}
+
+console.log(reduce([1, 2, 3, 5], function(a, b) {
+  return a + b;
+}, Math.random(0));
